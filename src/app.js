44d915a71a615fe78b3c03485df3a91bd9704eea
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-28 16:12:27 +0200

    improve cors handling
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -9,9 +9,17 @@ const URL_BASE = process.env.URL_BASE;
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://127.0.0.1:3001'
-}));
+const corsOptions = {
+  "origin": [
+    /^http:\/\/localhost(:\d+)?$/,    // Matches localhost with any port
+    /^http:\/\/127\.0\.0\.1(:\d+)?$/  // Matches loopback IP with any port
+  ],
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,         // If true cors call next to pass to following middlewares instead replying itself, is only usefull to implement more logic that the cors module correct response
+  "optionsSuccessStatus": 200         // Essential for older browser engines and preflight stability
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded());
